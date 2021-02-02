c298ea386e3e2be12c42e0f56a1ccc27a49be144
Reid <Reid.Finn@yahoo.com>
2021-02-02 15:50:55 -0600

    logout done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,13 @@
 class SessionsController < ApplicationController
 
-def welcome
-end
+    def welcome
+    end
 
+    def destroy
+        session.delete(:user_id)
+        redirect_to '/'
+    end
+
+    def new
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
     has_many :reviews
     has_many :reviewed_pizzas, through: :reviews, source: :pizza
     has_many :pizzas
+
+    has_secure_password
+    validates :username, uniqueness: true, presence: true
 end
