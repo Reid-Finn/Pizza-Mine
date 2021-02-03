1cbf39e3cb6a4a586ced8d05f59c1e3aad349d2c
Reid <Reid.Finn@yahoo.com>
2021-02-03 11:21:08 -0600

    app controller helper methods added

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,34 @@
 class ReviewsController < ApplicationController
+    before_action :redirect_if_not_logged_in
+
 
     def new
-        @pizza = Pizza.find_by_id(params[:id])
-        @review = @pizza.build_review
+      if @pizza = Pizza.find_by_id(params[:pizza_id])
+        @review = @pizza.reviews.build
+      else
+        @review = Review.new
+      end
     end
 
+
     def index
+    end
+
+
+    def create
+        @review = current_user.reviews.build(review_params)
+        if @review.save
+          redirect_to review_path(@review)
+        else
+          render :new
+        end
+      end
+
+
+    private
+
+    def set_pizza
+       @pizza = Pizza.find_by_id(params[:pizza_id])
     end
 
 end
