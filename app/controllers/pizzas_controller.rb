2a1674a12e4b523342dd1a22c2678653c6b44979
Reid <Reid.Finn@yahoo.com>
2021-02-02 18:59:33 -0600

    new and create for Pizza controller

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,4 +1,23 @@
 class PizzasController < ApplicationController
     def new
+        @pizza = Pizza.new
+        @pizza.build_company
+    end
+
+    def create
+        @pizza = Pizza.new(pizza_params)
+        @pizza.user_id = session[:user_id]
+        if @pizza.save
+            redirect_to pizza_path(@pizza)            
+        else
+            render :new
+        end
+
+    end
+
+    private
+    
+    def pizza_params
+        params.require(:pizza).permit(:type, :description, :company_id, company_attributes: [:name])
     end
 end
--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -3,4 +3,5 @@ class Pizza < ApplicationRecord
   belongs_to :company
   has_many :reviews
   has_many :users, through: :reviews
+  accepts_nested_attributes_for :company
 end
